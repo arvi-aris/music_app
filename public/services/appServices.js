8f052e1c58f137cb8bedca5c92db396acafeb9e5
Aravindh Nagarajan <aravindh.nagarajan@hotmail.com>
2017-05-21 16:34:55 +0530

    deployment changes

--- public/services/appServices.js
+++ public/services/appServices.js
@@ -1,6 +1,6 @@
 App
 .factory('Track_service', ['$resource', function ($resource) {
-    return $resource('https://104.197.128.152:8000/v1/tracks/:id',
+    return $resource('104.197.128.152:8000/v1/tracks/:id',
                     {method: 'POST', isArray: false },
                     {'query': { method: 'GET' }},
                     {'save': { method: 'POST' }},
@@ -10,7 +10,7 @@ App
                     );
 }])
 .factory('Genre_service', ['$resource', function ($resource) {
-    return $resource('https://104.197.128.152:8000/v1/genres/:id',
+    return $resource('104.197.128.152:8000/v1/genres/:id',
                     {method: 'POST', isArray: false },
                     {'query': { method: 'GET' }},
                     {'save': { method: 'POST' }},
