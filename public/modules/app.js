30e546e66e7a8ce1b538dcaf08facff871eaff8b
Aravindh Nagarajan <aravindh.nagarajan@hotmail.com>
2017-05-21 21:23:17 +0530

    loading bar added

--- public/modules/app.js
+++ public/modules/app.js
@@ -1,1 +1,1 @@
-var App = angular.module('music_app',['ngRoute','ngResource','angular-growl']);
+var App = angular.module('music_app',['ngRoute','ngResource','angular-growl','angular-loading-bar']);
