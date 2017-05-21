b961eeda0b2ea073fa9e11a6af1e0951c9a00cfc
Aravindh Nagarajan <aravindh.nagarajan@hotmail.com>
2017-05-21 16:04:12 +0530

    configured port

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const port = process.env.PORT || 8080;
 app.use(express.static('public'));
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-app.listen(8080,function(){
+app.listen(port,function(){
    	console.log('listening....')
 });
